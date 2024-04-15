9da8a49a81957c9f08564b0e52bc05bf1fd10bcc
IT21057892 <99260819+IT21057892@users.noreply.github.com>
2024-04-15 20:37:11 +0530

    commit

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,37 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// eslint-disable-next-line no-unused-vars
+import React from "react";
+import "./App.css";
+import SpeechRecognition, {
+  useSpeechRecognition,
+} from "react-speech-recognition";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const startListening = () =>
+    SpeechRecognition.startListening({
+      continuous: true,
+      language: "en",
+    });
+  const { transcript, browserSupportsSpeechRecognition } =
+    useSpeechRecognition();
+  if (!browserSupportsSpeechRecognition) {
+    return null;
+  }
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="containor">
+        <h2>Speech to Text Converter</h2>
+        <br />
+        <div className="main-content">{transcript}</div>
+        <div className="btn-style">
+          <button>Copy</button>
+          <button onClick={startListening}>Start Listning</button>
+          <button onClick={SpeechRecognition.stopListening()}>
+            Stop Listning
+          </button>
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
